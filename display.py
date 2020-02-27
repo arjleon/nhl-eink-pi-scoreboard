c9d0a69baaca5b7bca11a2b1e1078f3fe4246994
Armando Leon <ajlelorriaga@gmail.com>
2020-02-26 23:19:02 -0800

    Updated main and display for basic operations.

---
+++ display.py
@@ -0,0 +1,69 @@
+# noinspection PyUnresolvedReferences
+from waveshare_epd import epd2in9bc
+import logging
+from time import sleep
+from PIL import Image, ImageDraw, ImageFont
+import os
+import sys
+
+libraries = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'libs')
+if os.path.exists(libraries):
+    sys.path.append(libraries)
+images = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'imgs')
+
+
+class Display:
+    def __init__(self):
+        self.epd = epd2in9bc.EPD()
+        self.log = logging
+        self.log.basicConfig(level=logging.DEBUG)
+        self.is_initialized = False
+        self.font_big = ImageFont.truetype(os.path.join(images, 'Font.ttc'), 24)
+
+    def start(self):
+        if self.is_initialized:
+            self.log.error('Cannot start; already initialized')
+            return
+        if self.epd:
+            self.log.info('Starting')
+            self.is_initialized = True
+            self.epd.init()
+            sleep(1)
+
+    def stop(self):
+        if not self.is_initialized:
+            self.log.error('Cannot stop; not initialized')
+            return
+        if self.epd:
+            self.log.info('Stopping')
+            self.epd.sleep()
+            self.is_initialized = False
+            sleep(1)
+
+    def clear(self):
+        if self.epd:
+            self.log.info('Clearing')
+            self.epd.Clear()
+            sleep(1)
+
+    def show_finished_game(self, score_away, score_home):
+        b = self.__new_image()
+        canvas_b = self.__new_canvas(b)
+        canvas_b.text((20, 20), f'{score_away} - {score_home}', font=self.font_big, fill=0)
+        canvas_b.text((20, 50), 'Final', font=self.font_big, fill=0)
+
+        ry = self.__new_image()
+
+        self.__update(b, ry)
+
+    def __new_image(self):
+        return Image.new('1', (self.epd.height, self.epd.width), 255)
+
+    def __new_canvas(self, image):
+        return ImageDraw.Draw(image)
+
+    def __update(self, b, ry):
+        if self.epd and b and ry:
+            self.epd.display(self.epd.getbuffer(b), self.epd.getbuffer(ry))
+        else
+            self.log.error(f'Missing requirements for the display: ({self.epd}, {b}, {ry})')
--- main.py
+++ main.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-import sys
-import os
 
-imgs = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'imgs')
-libs = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'libs')
-if os.path.exists(libs):
-    sys.path.append(libs)
 
 import constants
 from datetime import datetime, timedelta
-from PIL import Image,ImageDraw,ImageFont
 import json
 import requests
 from os import path
 from time import sleep
 from game import Game, GameStatus
+from display import Display
 import utils
-from waveshare_epd import epd2in9bc
-import logging
-import time
 
 
 def write_file(filename, content):
@@ -95,7 +86,7 @@ def print_game_info(g, daysahead):
 
     if g.status == GameStatus.FINAL:
         expanded_status = 'Final: %d - %d' % (g.away.score, g.home.score)
-
+        display.show_finished_game(g.away.score, g.home.score)
     else:
         day, time = utils.get_friendly_local_date(g, daysahead)
         expanded_status = f'{day} {time}'
@@ -105,39 +96,14 @@ def print_game_info(g, daysahead):
     print(f'({away_record}) {id_to_abbr[g.away.id]} @ {id_to_abbr[g.home.id]} ({home_record})\n{expanded_status}')
 
 
+display = Display()
+display.start()
+display.clear()
+
 id_to_abbr = get_abbreviations(get_teams())
-#team_id = get_team_id('VGK')
-#game, days_ahead = get_next_game(team_id, datetime.today() - timedelta(days=1))
-#print_game_info(game, days_ahead)
+team_id = get_team_id('VGK')
+game, days_ahead = get_next_game(team_id, datetime.today() - timedelta(days=1))
+print_game_info(game, days_ahead)
 
-def test_display():
-    logging.basicConfig(level=logging.DEBUG)
-    epd = epd2in9bc.EPD()
-
-    logging.info("init and Clear")
-    epd.init()
-    epd.Clear()
-    time.sleep(1)
-
-    font24 = ImageFont.truetype(os.path.join(imgs, 'Font.ttc'), 24)
-    # font18 = ImageFont.truetype(os.path.join(imgs, 'Font.ttc'), 18)
-
-    logging.info("Drawing")
-    b = Image.new('1', (epd.height, epd.width), 255)
-    canvas_b = ImageDraw.Draw(b)
-    canvas_b.text((20, 20), 'Golden Knights', font=font24, fill=0)
-
-    ry = Image.new('1', (epd.height, epd.width), 255)
-
-    epd.display(epd.getbuffer(b), epd.getbuffer(ry))
-    time.sleep(4)
-
-    logging.info("Clear...")
-    epd.init()
-    epd.Clear()
-
-    logging.info("Goto Sleep...")
-    epd.sleep()
-
-
-test_display()
+display.clear()
+display.stop()
